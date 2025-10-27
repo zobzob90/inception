3f75deb7135c280eae77028d573b5c98b8cfb5b4
Eric <eric.trignau@gmail.com>
2025-10-27 10:58:11 +0100

    push docker nginx

---
+++ srcs/requirements/nginx/tools/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+openssl req -x509 -nodes \
+	-out /etc/nginx/ssl/inception.crt \
+	-keyout /etc/nginx/ssl/inception.key \
+	-subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=ertrigna.42.fr" \
+	-days 365 \
+	-newkey rsa:4096
+
+chmod 644 /etc/nginx/ssl/inception.crt
+chmod 644 /etc/nginx/ssl/inception.key
+
+exec nginx -g "daemon off;"
