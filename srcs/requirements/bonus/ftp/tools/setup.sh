64ebf3335a0549f7bcab848fa56cfdb8dbf53cb4
zobzob90 <eric.trignau@gmail.com>
2025-11-12 15:26:32 +0000

    all good

--- srcs/requirements/bonus/ftp/tools/setup.sh
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -e
 
-adduser -D -h /var/www/html ftpuser
-echo "ftpuser:ftppass" | chpasswd
+# Create user only if it doesn't exist
+if ! id -u ftpuser > /dev/null 2>&1; then
+    adduser -D -h /var/www/html ftpuser
+    echo "ftpuser:ftppass" | chpasswd
+fi
 
-exec vsftp /etc/vsftpd/vsftpd.conf
+# Set proper permissions for WordPress directory
+chown -R ftpuser:ftpuser /var/www/html
+chmod -R 755 /var/www/html
+
+exec vsftpd /etc/vsftpd/vsftpd.conf
