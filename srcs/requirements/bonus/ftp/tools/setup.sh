44558b28cca0e8117b76159b94b56bcf476e9ff3
Eric Trignau <ertrigna@f2r4s8.clusters.42paris.fr>
2025-11-27 18:50:47 +0100

    demain je push

--- srcs/requirements/bonus/ftp/tools/setup.sh
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -4,11 +4,11 @@ set -e
 # Create user only if it doesn't exist
 if ! id -u ftpuser > /dev/null 2>&1; then
     adduser -D -h /var/www/html ftpuser
-    echo "ftpuser:ftppass" | chpasswd
+    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 fi
 
 # Set proper permissions for WordPress directory
 chown -R ftpuser:ftpuser /var/www/html
-chmod -R 755 /var/www/html
+chmod -R 775 /var/www/html
 
 exec vsftpd /etc/vsftpd/vsftpd.conf
